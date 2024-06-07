f7537db0eb952887c45ec8fddbde2f2e7beab706
butterrflyboi <109606057+butterrflyboi@users.noreply.github.com>
2024-06-07 15:14:13 +0300

    Add files via upload
    В этой версии были написаны два public метода, возвращающих сумму и разность

--- practice/src/main/java/ru/neoflex/practice/controller/CalcController.java
+++ practice/src/main/java/ru/neoflex/practice/controller/CalcController.java
@@ -1,5 +1,19 @@
 package ru.neoflex.practice.controller;
-@RestController
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalcController {
+
+    @GetMapping("/plus/{a}/{b}")
+    public int add(@PathVariable("a") Integer a, @PathVariable("b") Integer b) {
+        return a + b;
+    }
+
+    @GetMapping("/minus/{a}/{b}")
+    public int subtract(@PathVariable("a") Integer a, @PathVariable("b") Integer b) {
+        return a - b;
+    }
 }
